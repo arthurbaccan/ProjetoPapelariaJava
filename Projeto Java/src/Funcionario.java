20576ec875a05ed42ae8d98e9f9524129071d416
aluno <natanfonatan13x@gmail.com>
2024-05-20 14:34:21 -0300

    musdansas

--- Projeto Java/src/Funcionario.java
+++ Projeto Java/src/Funcionario.java
@@ -1,18 +1,18 @@
 public class Funcionario extends Pessoa implements IPessoa{
 
-    private String edereco;
+    private String endereco;
     private int codigoDeRegistro;
     private double salario;
 
-    public Funcionario(String nome, int idade, String edereco, int codigoDeRegistro, double salario) {
+    public Funcionario(String nome, int idade, String endereco, int codigoDeRegistro, double salario) {
         super(nome, idade);
-        this.edereco = edereco;
+        this.endereco = endereco;
         this.codigoDeRegistro = codigoDeRegistro;
         this.salario = salario;
     }
 
     @Override
     public void exibir() {
-
+        System.out.println("\nFuncionário: "+nome+"\nIdade: "+idade+"\nEndereco: "+endereco+"\nCodigo de Registro: "+codigoDeRegistro+"\nSalario: "+salario);
     }
 }
